ca4379886a24d817b441b265b3409509347f6f47
Attila Surányi <attila.suranyi@protonmail.com>
2019-11-04 13:44:24 +0100

    initial commit

---
+++ stock-app-backend/src/main/java/com/codecool/stockapp/StockAppApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.stockapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockAppApplication.class, args);
+    }
+
+}
---
+++ stock-app-backend/src/test/java/com/codecool/stockapp/StockAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.stockapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
