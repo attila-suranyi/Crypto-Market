691bcd403f2f425007b3fb7ae7b171b97de671e1
danielKovacs94 <dankovacs94@gmail.com>
2019-11-08 13:34:58 +0100

    HOTFIX

--- stock-app-frontend/src/components/BuyCrypto.js
+++ stock-app-frontend/src/components/BuyCrypto.js
@@ -16,45 +16,46 @@ export default class BuyCrypto extends Component {
     }
 
     handleChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
+        this.setState({ [event.target.name]: event.target.value });}
 
 
-        handleSubmit = event => {
-            event.preventDefault();
+    handleSubmit = event => {
+        event.preventDefault();
 
-            Axios.post(`http://localhost:8080/buy`, {
-                symbol: this.props.symbol,
-                price: this.context.currentCryptoData,
-                amount: this.state.amount,
-                total: this.state.amount * this.context.currentCryptoData
+        Axios.post(`http://localhost:8080/buy`, {
+            symbol: this.props.symbol,
+            price: this.context.currentCryptoData,
+            amount: this.state.amount,
+            total: this.state.amount * this.context.currentCryptoData
+        })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
             })
-                .then(res => {
-                    console.log(res);
-                    console.log(res.data);
-                })
-        }
+    }
+    
 
-        render() {
-            return (
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="formPrice">
-                        <Form.Label>Price</Form.Label>
-                        <Form.Control value={this.context.currentCryptoData} />
-                    </Form.Group>
-                    <Form.Group controlId="formAmount">
-                        <Form.Label>Amount</Form.Label>
-                        <Form.Control placeholder="Amount" onChange={this.handleChange} name="amount" />
-                    </Form.Group>
-                    <Form.Group controlId="formTotal">
-                        <Form.Label>Total</Form.Label>
-                        <Form.Control placeholder="Total" value={this.context.currentCryptoData * this.state.amount} onChange={this.handleChange} name="total" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Buy
+    render() {
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Form.Group controlId="formPrice">
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control value={this.context.currentCryptoData} />
+                </Form.Group>
+                <Form.Group controlId="formAmount">
+                    <Form.Label>Amount</Form.Label>
+                    <Form.Control placeholder="Amount" onChange={this.handleChange} name="amount" />
+                </Form.Group>
+                <Form.Group controlId="formTotal">
+                    <Form.Label>Total</Form.Label>
+                    <Form.Control placeholder="Total" value={this.context.currentCryptoData * this.state.amount} onChange={this.handleChange} name="total" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Buy
                 </Button>
-                </Form>
-            )
-        }
+            </Form>
+        )
     }
 }
+
 
