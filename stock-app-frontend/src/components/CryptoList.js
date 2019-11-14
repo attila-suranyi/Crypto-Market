acaf5d008e2042b8b6530a63e006002f70e97bf3
Attila Surányi <attila.suranyi@protonmail.com>
2019-11-14 09:58:29 +0100

    sorts to one direction

--- stock-app-frontend/src/components/CryptoList.js
+++ stock-app-frontend/src/components/CryptoList.js
@@ -7,24 +7,36 @@ import CryptoItem from './CryptoItem';
 export default class CryptoList extends Component {
     static contextType = CryptoDataContext;
 
-    getQueryParam() {
-        /*const queryString = require("query-string");
-        const parsed = queryString.parse(this.props.location.search);
-        return parsed.sort_by;*/
+    state = {
+        sort_dir: "asc"
+    }
 
+    getQueryParam(key) {
         let search = window.location.search;
         let params = new URLSearchParams(search);
-        let sort_by = params.get('sort_by');
-        return sort_by
+        let param = params.get(key);
+        return param
     }
 
     sortByProperty = () => {
-        let URL = "http://localhost:8080/sorted?sort_by=" + this.getQueryParam() + "&sort_dir=desc";
+
+        let URL = "http://localhost:8080/sorted?sort_by=" + this.getQueryParam("sort_by") 
+                    + "&sort_dir=" + this.getQueryParam("sort_dir");
+
         this.context.fetchAllCryptoData(URL);
+
+        if (this.state.sort_dir === "desc") {
+            this.setState({sort_dir: "asc"})
+            console.log(this.state.sort_dir)
+            return;
+        } else {
+            this.setState({sort_dir: "desc"})
+            return;
+        } 
     }
 
     componentDidMount() {
-        if (this.getQueryParam()) {
+        if (this.getQueryParam("sort_dir")) {
             this.sortByProperty();
         }
     }
@@ -37,7 +49,8 @@ export default class CryptoList extends Component {
                         <tr>
                             <th><p>#</p></th>
                             <th><p>Name</p></th>
-                            <th><p><Link to="/sorted?sort_by=symbol" >Symbol</Link></p></th>
+                            <th><p><Link to={`/sorted?sort_by=symbol&sort_dir=${this.state.sort_dir}`} 
+                                        >Symbol</Link></p></th>
                             <th><p>Price</p></th>
                             <th><p>Change (24h)</p></th>
                             <th><p>Market Cap</p></th>
