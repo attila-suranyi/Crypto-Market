06ac366de2e1988e695cdc372a5fed7a5e07a3b2
danielKovacs94 <dankovacs94@gmail.com>
2019-11-19 13:18:23 +0100

    add flexbox to tradeCrypto

--- stock-app-frontend/src/components/BuyCrypto.js
+++ stock-app-frontend/src/components/BuyCrypto.js
@@ -19,56 +19,59 @@ export default class BuyCrypto extends Component {
   handleSubmit = event => {
     event.preventDefault();
 
-    let order = {
+    let transaction = {
       symbol: this.props.symbol,
       price: this.context.singleCryptoData.quote.usd.price,
       amount: this.state.amount,
       total: this.state.amount * this.context.singleCryptoData.quote.usd.price
     };
 
-    this.context.sendDataToBackend("http://localhost:8080/buy", order);
+    this.context.sendDataToBackend("http://localhost:8080/buy", transaction);
   };
 
   render() {
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group controlId="formPrice">
-          <Form.Label>Price</Form.Label>
-          <Form.Control
-            readOnly
-            value={
-              this.context.singleCryptoData.quote
-                ? this.context.singleCryptoData.quote.usd.price
-                : 0
-            }
-          />
-        </Form.Group>
-        <Form.Group controlId="formAmount">
-          <Form.Label>Amount</Form.Label>
-          <Form.Control
-            placeholder="Amount"
-            onChange={this.handleChange}
-            name="amount"
-          />
-        </Form.Group>
-        <Form.Group controlId="formTotal">
-          <Form.Label>Total</Form.Label>
-          <Form.Control
-            readOnly
-            placeholder="Total"
-            value={
-              this.context.singleCryptoData.quote
-                ? this.context.singleCryptoData.quote.usd.price *
+      <div>
+        <div><p>{this.props.tradeDir} {this.props.symbol}</p></div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formPrice">
+            <Form.Label>Price</Form.Label>
+            <Form.Control
+              readOnly
+              value={
+                this.context.singleCryptoData.quote
+                  ? this.context.singleCryptoData.quote.usd.price
+                  : 0
+              }
+            />
+          </Form.Group>
+          <Form.Group controlId="formAmount">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control
+              placeholder="Amount"
+              onChange={this.handleChange}
+              name="amount"
+            />
+          </Form.Group>
+          <Form.Group controlId="formTotal">
+            <Form.Label>Total</Form.Label>
+            <Form.Control
+              readOnly
+              placeholder="Total"
+              value={
+                this.context.singleCryptoData.quote
+                  ? this.context.singleCryptoData.quote.usd.price *
                   this.state.amount
-                : 0
-            }
-            name="total"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Buy
-        </Button>
-      </Form>
+                  : 0
+              }
+              name="total"
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            {this.props.tradeDir}
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
