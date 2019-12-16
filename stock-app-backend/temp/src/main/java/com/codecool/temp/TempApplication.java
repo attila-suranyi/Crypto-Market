924a891ca433483a09abf06b4c1f067d5bfec6df
Attila Surányi <attila.suranyi@protonmail.com>
2019-12-16 15:35:43 +0100

    loadBalance fixed

--- stock-app-backend/temp/src/main/java/com/codecool/temp/TempApplication.java
+++ stock-app-backend/temp/src/main/java/com/codecool/temp/TempApplication.java
@@ -2,14 +2,25 @@ package com.codecool.temp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 public class TempApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TempApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
