16eda98a6e75eba8f0988102c988f265bed0997d
Joó Norbert <joonorbi12@gmail.com>
2019-12-17 15:07:11 +0100

    added Transactional annotation to updateBalance query

--- stock-app-backend/user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
+++ stock-app-backend/user-service/src/main/java/com/codecool/userservice/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 
@@ -14,6 +15,7 @@ public interface UserRepository extends JpaRepository<StockAppUser, Long> {
 
     Optional<StockAppUser> findByUserName(String userName);
 
+    @Transactional
     @Query("update StockAppUser a set a.balance = :balance where a.id = :id")
     @Modifying(clearAutomatically = true)
     void updateBalance(@Param("balance") Double balance, @Param("id") Long id);
