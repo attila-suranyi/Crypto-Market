fdbd9ed41e2749823906e9221c41a45a3b05a068
Joó Norbert <joonorbi12@gmail.com>
2019-12-19 09:33:42 +0100

    added ApiGatewayApplication

---
+++ stock-app-backend/api-gateway/src/main/java/com/codecool/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.codecool.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+@EntityScan( basePackages = {"com.codecool.cryptomarketjsonclasses"} )
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+}
