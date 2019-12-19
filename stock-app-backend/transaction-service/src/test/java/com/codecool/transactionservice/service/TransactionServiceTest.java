0d0ca7881728464961f376909f8dd91342d8a313
Attila Surányi <attila.suranyi@protonmail.com>
2019-12-19 11:13:20 +0100

    small refactor and access modifiers set for tests

--- stock-app-backend/transaction-service/src/test/java/com/codecool/transactionservice/service/TransactionServiceTest.java
+++ stock-app-backend/transaction-service/src/test/java/com/codecool/transactionservice/service/TransactionServiceTest.java
@@ -8,9 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
